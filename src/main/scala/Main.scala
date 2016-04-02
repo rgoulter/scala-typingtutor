d703bb68b3c2fb5bda97c9605e1475ca714f7559
Richard Goulter <richard.goulter@gmail.com>
2016-04-02 20:48:17 +0800

    Restrict the text area movement to just the typing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,10 @@ import java.awt.event.KeyListener
 import java.awt.event.KeyEvent
 import com.rgoulter.typingtutor.PartialTokenMaker
 import org.fife.ui.rsyntaxtextarea.modes.JavaTokenMaker
+import javax.swing.event.CaretListener
+import javax.swing.event.CaretEvent
+import java.awt.event.MouseListener
+import java.awt.event.MouseEvent
 
 class TextEditorDemo extends JFrame {
   val SampleText = """public class HelloWorld {
@@ -33,6 +37,7 @@ class TextEditorDemo extends JFrame {
 
   textArea.setCaretPosition(0)
 
+
   // To save time, the text area only updates when
   // things change. This isn't ideal.
   //
@@ -44,8 +49,20 @@ class TextEditorDemo extends JFrame {
     textArea.setCaretPosition(pos)
   }
 
-  textArea.addKeyListener(new KeyListener {
-    override def keyPressed(ke: KeyEvent): Unit = {}
+  val typeTutorKL = new KeyListener {
+    override def keyPressed(ke: KeyEvent): Unit = {
+      ke.getKeyCode() match {
+        case KeyEvent.VK_LEFT  => ke.consume()
+        case KeyEvent.VK_RIGHT => ke.consume()
+        case KeyEvent.VK_UP    => ke.consume()
+        case KeyEvent.VK_DOWN  => ke.consume()
+        case KeyEvent.VK_END   => ke.consume()
+        case KeyEvent.VK_HOME  => ke.consume()
+        case KeyEvent.VK_PAGE_UP   => ke.consume()
+        case KeyEvent.VK_PAGE_DOWN => ke.consume()
+        case _ => ()
+      }
+    }
     override def keyReleased(ke: KeyEvent): Unit = {}
     override def keyTyped(ke: KeyEvent): Unit = {
       val caretPosition = textArea.getCaretPosition()
@@ -63,7 +80,7 @@ class TextEditorDemo extends JFrame {
           // ignore
         }
         case c => { // what about characters like 'Home'?
-          println(s"Pressed Key '$charAtPos':$caretPosition <= '$pressedChar'")
+//          println(s"Pressed Key '$charAtPos':$caretPosition <= '$pressedChar'")
 
           if (caretPosition + 1 < textArea.getText().length()) {
             textArea.setCaretPosition(caretPosition + 1)
@@ -74,7 +91,24 @@ class TextEditorDemo extends JFrame {
 
       partialTokMak.position = textArea.getCaretPosition()
     }
-  })
+  }
+
+  val typeTutorML = new MouseListener {
+    def mousePressed(me: MouseEvent): Unit = {
+      me.consume()
+    }
+    def mouseReleased(me: MouseEvent): Unit = {
+      me.consume()
+    }
+    def mouseClicked(me: MouseEvent): Unit = {
+      me.consume()
+    }
+    def mouseEntered(me: MouseEvent): Unit = {}
+    def mouseExited(me: MouseEvent): Unit = {}
+  }
+
+  textArea.addKeyListener(typeTutorKL)
+  for (ml <- textArea.getMouseListeners()) { textArea.removeMouseListener(ml) }
 
   val sp = new RTextScrollPane(textArea)
   cp.add(sp)
