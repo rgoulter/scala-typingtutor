24c8a14355263d825089bfabd404f28d7510e8ac
Richard Goulter <richard.goulter@gmail.com>
2016-04-03 21:25:40 +0800

    Marker movements modelled in FRP

--- src/main/scala/com/rgoulter/typingtutor/TypingKeyListener.scala
+++ src/main/scala/com/rgoulter/typingtutor/TypingKeyListener.scala
@@ -8,6 +8,10 @@ import sodium.CellSink
 import sodium.Stream
 import sodium.StreamSink
 
+sealed trait TypingEvent
+case class Backspace() extends TypingEvent
+case class TypedCharacter(val c: Char) extends TypingEvent
+
 class TypedStats(val numTotal: Int,
                  val numCorrect: Int,
                  val numIncorrect: Int,
@@ -41,13 +45,36 @@ class TypedStats(val numTotal: Int,
 }
 
 // callback: (position, numIncorrect) => ()
-class TypingKeyListener(var text: String,
-                        callback: (Int, Int) => Unit,
-                        endGame: TypedStats => Unit) extends KeyListener {
-  private var lastCorrectPos = 0
-  private var numIncorrect = 0
+class TypingKeyListener(var text: String) extends KeyListener {
+  /*
+   * So how do we do this?
+   *
+   * endGame becomes a Steam<Unit> we export.
+   *
+   * callback of (latestCorrect, numIncorrect) is a cell,
+   *   lifted from latestCorrect cell,
+   *               numIncorrect cell
+   *
+   * numTypedTotal might as well be a cell, also.
+   * 
+   * I'm not sure what the idiomatic way of getting mutKeyEntries is.
+   *
+   * Regardless, a Stream of (Expected, Got),
+   * had from [Backspace | Key c] event,
+   * & can get 'expected' by mapping from latestCorrect -> text ...
+   * 
+   * XXX The text is also a Cell, incidentally.
+   *
+   * stats... Hmm.
+   * Well, since we can't "get the value" from FRP system, right,
+   *  -- can 'lift' from the other values to get it,
+   * stats should be a cell, we take snapshot of this at time of game end, I guess?
+   */
 
-  private def pos: Int = lastCorrectPos + numIncorrect
+//  private var lastCorrectPos = 0
+//  private var numIncorrect = 0
+
+//  private def pos: Int = lastCorrectPos + numIncorrect
 
   private var numTypedTotal: Int = 0
   private var numTypedIncorrect: Int = 0
@@ -57,12 +84,72 @@ class TypingKeyListener(var text: String,
 
   // collect list-of (exp, actual, time)
   private val mutKeyEntries = new scala.collection.mutable.ArrayBuffer[(Char, Char, Long)](1000)
+  
+  
+  
+  private val typedEvents = new StreamSink[TypingEvent]
+  val backspaceEvents = typedEvents.filter({
+    case Backspace() => true
+    case _ => false
+  })
+  val typedCharEvents = typedEvents.filter({
+    case TypedCharacter(_) => true
+    case _ => false
+  }).map({
+    case TypedCharacter(c) => c
+    case _ => throw new IllegalStateException()
+  })
+
+  // lastCorrect is a cell. How?
+  val markers = typedEvents.accum[(Int, Int)]((0, 0), (te, pair) => {
+    val (numCorrect, numIncorrect) = pair
+
+    if (numIncorrect == 0) {
+      te match {
+        case Backspace() => {
+          // numCorrect >= 0
+          (Math.max(0, numCorrect - 1), numIncorrect)
+        }
+        case TypedCharacter(typedChar) => {
+          val expectedChar = text.charAt(numCorrect)
+
+          if (expectedChar == typedChar) {
+            // numCorrect < textSize
+            val textSize = text.size
+            (Math.min(numCorrect + 1, textSize - 1), numIncorrect)
+          } else {
+            (numCorrect, numIncorrect + 1)
+          }
+        }
+      }
+    } else {
+      te match {
+        case Backspace() => {
+          (numCorrect, numIncorrect - 1)
+        }
+        case TypedCharacter(t) => {
+          // **MAGIC** MaxIncorrectRule = 5
+          (numCorrect, Math.min(numIncorrect + 1, 5))
+        }
+      }
+    }
+  })
+  val numCorrect   = markers.map(_._1)
+  val numIncorrect = markers.map(_._2)
+
+  // TODO: Send 'quit' event when reached end of text; when send too many..
+  // e.g. **MAGIC** MaxCorrectTypedRule = 1000
+
+  val totalTypedCt = typedEvents.accum[Int](0, (_, n) => n + 1)
+
+  // So... backspace should decrease numIncorrect until 0; then decrease latestCorrect.
 
   private val endGameSink = new StreamSink[Unit]()
   val endGame: Stream[Unit] = endGameSink
 
   def stats: TypedStats = {
-    new TypedStats(numTypedTotal, numTypedCorrect, numTypedIncorrect, mutKeyEntries.toArray)
+    // XXX should be using FRP
+    new TypedStats(numTypedTotal, numTypedCorrect, numTypedIncorrect, Array())
   }
 
   override def keyPressed(ke: KeyEvent): Unit = {}
@@ -70,7 +157,6 @@ class TypingKeyListener(var text: String,
   override def keyReleased(ke: KeyEvent): Unit = {}
 
   override def keyTyped(ke: KeyEvent): Unit = {
-    val expectedChar = text.charAt(pos)
     val pressedChar = ke.getKeyChar
 
     pressedChar match {
@@ -79,49 +165,18 @@ class TypingKeyListener(var text: String,
       }
 
       case '\b' => {
-        if (numIncorrect > 0) {
-          numIncorrect -= 1
-        } else if (pos > 0) {
-          lastCorrectPos -= 1
-        }
+        typedEvents.send(Backspace())
       }
 
       // newlines.. are considered as just 'incorrect' characters.
       case c if !ke.isControlDown() => { // what about characters like 'Home'?
 //          println(s"Pressed Key '$charAtPos':$caretPosition <= '$pressedChar'")
-
-        if (pos + 1 < text.length()) {
-          // Keep track of entered things
-          // (Slight discrepancy if we include these all).
-          val timeMillis = System.currentTimeMillis()
-          val entry = (expectedChar, pressedChar, timeMillis)
-          mutKeyEntries += entry
-
-
-          numTypedTotal += 1
-
-          if (numIncorrect == 0 &&
-              pressedChar == expectedChar) {
-            lastCorrectPos += 1
-          } else if (numIncorrect < 5) { // **MAGIC** MaxIncorrectRule = 5
-            numIncorrect += 1
-            numTypedIncorrect += 1
-          }
-
-          if (numTypedCorrect > 1000) { // **MAGIC** MaxCorrectTypedRule = 1000
-            endGameSink.send(())
-          }
-        } else {
-          // Got to the end.
-          endGameSink.send(())
-        }
+        typedEvents.send(TypedCharacter(c))
 
         // The '}' still inserts a character, even if `editable` is false!
         ke.consume()
       }
       case _ => {}
     }
-
-    callback(lastCorrectPos, numIncorrect)
   }
 }
