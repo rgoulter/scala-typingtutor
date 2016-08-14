5028014111ba33a25688d9510550496bc83097ee
Richard Goulter <richard.goulter@gmail.com>
2016-08-14 15:13:24 +0800

    Move TypedStats to its own file

--- src/main/scala/com/rgoulter/typingtutor/TypingKeyListener.scala
+++ src/main/scala/com/rgoulter/typingtutor/TypingKeyListener.scala
@@ -29,45 +29,6 @@ case class State(val numCorrect: Int = 0,
   val position = numCorrect
 }
 
-class TypedStats(val numTotal: Int,
-                 val numCorrect: Int,
-                 val numIncorrect: Int,
-                 val entries: Array[(Char, Char, Long)]) {
-  def print(): Unit = {
-    println(s"Total: $numTotal")
-    println(s"Correct: $numCorrect")
-    println(s"Incorrect: $numIncorrect")
-  }
-
-  val duration: Long = {
-    if (!entries.isEmpty) {
-      val start = entries.head._3
-      val end   = entries.last._3
-      end - start
-    } else {
-      0
-    }
-  }
-
-  val durationInMins: Double =
-    (duration / 1000).toDouble / 60
-
-  val durationStr: String =
-    s"${(duration / 1000) / 60}:${(duration / 1000) % 60}"
-
-  val accuracy = numCorrect.toDouble / numTotal
-
-  val accuracyPercent: Int = (accuracy * 100).toInt
-
-  // wpm = (# chars / 5) / (time in mins)
-  // Rounded to int is close enough
-  val wpmStr =
-    if (durationInMins > 0)
-      ((numCorrect / 5) / durationInMins).toInt
-    else
-      "???"
-}
-
 // callback: (position, numIncorrect) => ()
 class TypingKeyListener(val text: Cell[Token]) extends KeyListener {
   Utils.dumpTokens(text.sample(), "TypKL text.sample Tokens")
