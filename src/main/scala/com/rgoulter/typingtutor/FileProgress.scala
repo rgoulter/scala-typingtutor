000dfce9890f5a714ca4d887d21742b218d63668
Richard Goulter <richard.goulter@gmail.com>
2016-08-17 00:12:21 +0800

    Update persistence API, wrap the SQL access to implement FileProgress

--- src/main/scala/com/rgoulter/typingtutor/FileProgress.scala
+++ src/main/scala/com/rgoulter/typingtutor/FileProgress.scala
@@ -52,10 +52,39 @@ class FileProgressEntry(val path: Path,
 
 
 
+/** CRUD class for persisting progress in the files the user practices with.
+  *
+  * Unless otherwise stated, all [[Path]]s given to this class should be
+  * relative to the source-files directory. (e.g. `./typingtutor/` by default)
+  */
 abstract class FileProgress {
-  def entries: List[FileProgressEntry]
-
   def addEntry(entry: FileProgressEntry): Unit
 
-  def removeEntry(path: String): Unit
+  def entries: List[FileProgressEntry]
+
+  def updateEntry(entry: FileProgressEntry, newOffset: Int): Unit
+
+  def removeEntry(path: Path): Unit
+
+  def offsetOf(path: Path): Option[Int] = {
+    entries.find(_.path == path).map(_.offset)
+  }
+
+  /** Given a list of paths (relative to e.g. ./typingtutor/; so,
+    * e.g. `List(sample/Hello.java)`),
+    * add entries for any files not previously persisted,
+    * remove entries for any files which aren't given.
+    */
+  def updateEntries(existingFilesInDir: List[Path]): Unit = {
+    val persistedPaths = entries.map(_.path).toSet
+    val onDiskPaths = existingFilesInDir.toSet
+
+    for (p <- onDiskPaths -- persistedPaths) {
+      addEntry(new FileProgressEntry(p, 0))
+    }
+
+    for (p <- persistedPaths -- onDiskPaths) {
+      removeEntry(p)
+    }
+  }
 }
--- src/main/scala/com/rgoulter/typingtutor/sql/FileProgressDB.scala
+++ src/main/scala/com/rgoulter/typingtutor/sql/FileProgressDB.scala
@@ -2,11 +2,16 @@ package com.rgoulter.typingtutor.sql
 
 import java.io.File
 import java.nio.file.Path
+import java.nio.file.Paths
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
 import java.sql.SQLException
 import java.sql.Statement
+
+import com.rgoulter.typingtutor.FileProgress
+import com.rgoulter.typingtutor.FileProgressEntry
+import com.rgoulter.typingtutor.FileProgressEntry
 
 
 
@@ -44,8 +49,14 @@ object SQLHelper {
 
 
 
-class FileProgressDB(connection: Connection, dir: File) {
+object RawFileProgressDB {
   type Entry = (String, Int)
+}
+
+
+
+class RawFileProgressDB(connection: Connection) {
+  import RawFileProgressDB.Entry
 
   val TableName = "fileoffsets"
   val FileColumn   = "file"
@@ -55,6 +66,13 @@ class FileProgressDB(connection: Connection, dir: File) {
 
   val CreateTblSql = s"CREATE TABLE IF NOT EXISTS $TableName ($FileColumn TEXT PRIMARY KEY, $OffsetColumn INTEGER)"
   statement.executeUpdate(CreateTblSql)
+
+  def addEntry(entry: Entry): Unit = {
+    val (path, offset) = entry
+    val Sql = s"INSERT OR REPLACE INTO $TableName VALUES ('$path', $offset)"
+
+    statement.executeUpdate(Sql)
+  }
 
   def entries: List[Entry] = {
     val QuerySql = s"SELECT ($FileColumn, $OffsetColumn) FROM $TableName"
@@ -72,9 +90,9 @@ class FileProgressDB(connection: Connection, dir: File) {
     res
   }
 
-  def addEntry(entry: Entry): Unit = {
+  def updateEntry(entry: Entry, newOffset: Int): Unit = {
     val (path, offset) = entry
-    val Sql = s"INSERT OR REPLACE INTO $TableName VALUES ('$path', $offset)"
+    val Sql = s"UPDATE $TableName SET $OffsetColumn = $newOffset WHERE $FileColumn = '$path'"
 
     statement.executeUpdate(Sql)
   }
@@ -83,5 +101,54 @@ class FileProgressDB(connection: Connection, dir: File) {
     val Sql = s"DELETE FROM $TableName WHERE $FileColumn = '$path'"
 
     statement.executeUpdate(Sql)
+  }
+}
+
+
+
+object FileProgressDB {
+  import RawFileProgressDB.Entry
+
+  def fpEntryFromEntry(e: Entry): FileProgressEntry = {
+    val (path, offset) = e
+
+    new FileProgressEntry(Paths.get(path), offset)
+  }
+
+  def entryFromFpEntry(fpE: FileProgressEntry): Entry = {
+    val pathStr = fpE.path.toString()
+    val offset  = fpE.offset
+
+    (pathStr, offset)
+  }
+}
+
+
+
+/** CRUD class for persisting progress in the files the user practices with.
+  * Stores using an SQLite database.
+  *
+  * Unless otherwise stated, all [[Path]]s given to this class should be
+  * relative to the source-files directory. (e.g. `./typingtutor/` by default)
+  */
+class FileProgressDB(connection: Connection) extends FileProgress {
+  import FileProgressDB.{ fpEntryFromEntry, entryFromFpEntry }
+
+  val rawDB = new RawFileProgressDB(connection)
+
+  override def addEntry(entry: FileProgressEntry): Unit = {
+    rawDB.addEntry(entryFromFpEntry(entry))
+  }
+
+  override def entries: List[FileProgressEntry] = {
+    rawDB.entries.map(fpEntryFromEntry)
+  }
+
+  override def updateEntry(entry: FileProgressEntry, newOffset: Int): Unit = {
+    rawDB.updateEntry(entryFromFpEntry(entry), newOffset)
+  }
+
+  override def removeEntry(path: Path): Unit = {
+    rawDB.removeEntry(path.toString())
   }
 }
